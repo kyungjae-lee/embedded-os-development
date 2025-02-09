ad1b3d5eea75a801a3926df516121d27e0b66a84
Kyungjae Lee <kjxlee@gmail.com>
2025-02-08 22:57:17 -0600

    feat: create uart input and output features and update 'Makefile' and 'boot/Main.c' to accommodate the changes

--- boot/Main.c
+++ boot/Main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "HalUart.h"
+#include "stdio.h"
 
 static void Hw_init(void);
 
@@ -12,6 +13,16 @@ void main(void)
 	while (i--)
 	{
 		Hal_uart_put_char('N');
+	}
+	
+	Hal_uart_put_char('\n');
+	putstr("Hello World!\n");
+
+	i = 100;
+	while (i--)
+	{
+		uint8_t ch = Hal_uart_get_char();
+		Hal_uart_put_char(ch);
 	}
 }
 
